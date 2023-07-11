8fb6c43b4734ff989530e865fb11e5d46b8f64c4
doanhtu07 <doanhtu07@gmail.com>
2023-07-11 18:48:11 -0500

    Init

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # backend-express-frame
+
+## Features
+
+- Typescript
+- ExpressJS Basic App
+- PostgreSQL database
+- Prisma ORM
+- Jest Testing
+- Auto Lint + Format
+
+## Introduction
+
+This repo is a basic ExpressJS app setup. Feel free to clone it whenever you do a new project.
+
+Besides the basic stuff in this repo, there are many more concerns you need to consider for production:
+
+- Security / Rate Limiting
+- Authentication
+- Error Tracking
+- Data Logging
+- Deploy
+- Cross Platform Resources (Monorepo)
+- ...
+
+But the good thing is no one needs to know everything in order to start. Just jump right into any project that inspires you and have fun!
+
+## Required Tools
+
+- Docker
+- VSCode
+- VSCode Plugins:
+  - Eslint
+  - Prettier
+  - Prisma
+  - Docker (Optional)
+
+## Env File
+
+Normally, you would want to gitignore the `.env` file. But for the sake of testing, I'll leave `.env` file right in the repo.
+
+## Setup
+
+1. To fully run the API `/about`, you need to have Docker installed.
+
+   a. After Docker is installed, run `yarn dock` to initialize database instance in Docker.
+
+   b. Then, run `yarn pdev` to apply schema changes to database.
+
+   c. Finally, run `yarn pgen` to generate types to use in Typescript code.
+
+2. If you just want to run the API without database, call `/about/no-db`.
+
+## Run
+
+1. Run `yarn install`.
+
+2. Run `yarn dev`.
---
+++ src/app.ts
@@ -0,0 +1,123 @@
+import cors from "cors";
+import express, { Express } from "express";
+import { corsOptions } from "./cors";
+import { ErrorWrap } from "./utils/Error";
+import { User } from "@prisma/client";
+import { convertExpressUser } from "./utils/Types";
+import * as http from "http";
+import prisma from "./prisma/client";
+import bodyParser from "body-parser";
+
+export default class App {
+  server: http.Server | null | undefined = null;
+  app: Express | null | undefined = null;
+
+  constructor() {
+    this.app = express();
+
+    this.app.use(cors(corsOptions));
+
+    this.app.get("/about", bodyParser.json(), (req, res) => {
+      const { userId } = req.query as { userId: string | null };
+
+      prisma.user
+        .findUnique({
+          where: userId
+            ? { id: BigInt(userId) }
+            : { email: "doanhtu07@gmail.com" },
+        })
+        .then(function createUserIfNone(user) {
+          if (!user) {
+            return prisma.user.create({
+              data: {
+                email: "doanhtu07@gmail.com",
+                metadata: JSON.stringify({
+                  name: "Anh Tu Do",
+                  major: "Computer Science",
+                  class: "Junior",
+                  school: "University of Texas at Dallas",
+                  hobbies: [
+                    "coding",
+                    "singing",
+                    "startup",
+                    "system design",
+                    "exercise",
+                  ],
+                }),
+              },
+            });
+          }
+
+          return user;
+        })
+        .then(function resolve(user) {
+          res.send({
+            user: convertExpressUser(user),
+          });
+        })
+        .catch((err) => {
+          console.error(ErrorWrap(err));
+        });
+    });
+
+    this.app.get("/about/no-db", (req, res) => {
+      const fakeUser: User = {
+        id: BigInt(-1),
+        email: "doanhtu07@gmail.com",
+        metadata: JSON.stringify({
+          name: "Anh Tu Do",
+          major: "Computer Science",
+          class: "Junior",
+          school: "University of Texas at Dallas",
+          hobbies: [
+            "coding",
+            "singing",
+            "startup",
+            "system design",
+            "exercise",
+          ],
+        }),
+      };
+
+      res.send({
+        user: convertExpressUser(fakeUser),
+      });
+    });
+
+    this.app.post("/about/update", bodyParser.json(), (req, res) => {
+      const { id, newEmail } = req.body as { id: string; newEmail: string };
+
+      prisma.user
+        .update({
+          where: { id: BigInt(id) },
+          data: { email: newEmail },
+        })
+        .then(function resolve(user) {
+          res.send({
+            user: convertExpressUser(user),
+          });
+        })
+        .catch((err) => {
+          console.error(ErrorWrap(err));
+        });
+    });
+
+    // custom 404
+    this.app.use((req, res) => {
+      res.status(404).send({
+        message: "Sorry can't find route!",
+      });
+    });
+  }
+
+  start(): http.Server | null | undefined {
+    this.server = this.app?.listen(process.env.PORT, () => {
+      console.log(`Server started at port ${process.env.PORT}.`);
+    });
+    return this.server;
+  }
+
+  stop(): void {
+    this.server?.close();
+  }
+}
